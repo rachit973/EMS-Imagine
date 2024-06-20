67c78f7365fb3fbe1c09a809c377a7a3b5b356de
rachit <118990869+rachit973@users.noreply.github.com>
2024-06-20 11:37:18 +0530

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -44,6 +44,10 @@ app.post("/login", (req, res) => {
     res.status(200).json({ message: "Logged in as ${role}" });
 });
 
+app.post("/loginforma",(req,res) => {
+    const { role } =req.body;
+    res.status(200).json({ message: "logged into admin" });
+});
 
 app.get("/admins", async (req, res) => {
     try {
