223abeb677a67a7ade9c959fe3bf3b896cbe1199
rachit <118990869+rachit973@users.noreply.github.com>
2024-06-24 16:20:02 +0530

    Update LeaveApplicationE.jsx

--- EMS/src/components/LeaveApplicationE.jsx
+++ EMS/src/components/LeaveApplicationE.jsx
@@ -23,7 +23,7 @@ function LeaveApplicationE() {
     const confirmDelete = window.confirm("Are you sure you want to delete this leave application?");
     if (confirmDelete) {
     try {
-      await axios.delete(`http://localhost:8001/leaves/${id}`);
+      await axios.delete(`https://ems-imagine.onrender.com/leaves/${id}`);
       // Update the state to remove the deleted leave application
       setLeaves(leaves.filter(leave => leave._id !== id));
     } catch (error) {
