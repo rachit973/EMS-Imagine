65674cb13d01dbd5e7bc498ea861f31838d43f14
rachit <118990869+rachit973@users.noreply.github.com>
2024-06-24 15:13:49 +0530

    Update Loginformjj

--- EMS/src/components/LoginformE.jsx
+++ EMS/src/components/LoginformE.jsx
@@ -1,3 +1,147 @@
+// import "../styles/Login.css";
+// import axios from "axios";
+// import React, { useState, useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+
+// function LoginformE() {
+//   const [lformData, setFormData] = useState({
+//     uid: "",
+//     password: "",
+//   });
+//   const [loginData, setLoginData] = useState([]);
+//   const navigate = useNavigate();
+//   localStorage.removeItem("isLoggedIn");
+//   window.localStorage.removeItem("isLoggedIn");
+//   axios.defaults.withCredentials = true;
+
+//   useEffect(() => {
+//     const fetchAllAdmin = async () => {
+//       try {
+//         const res = await axios.get("https://ems-imagine.onrender.com");
+//         setLoginData(res.data);
+//         console.log(res);
+//       } catch (err) {
+//         console.log(err);
+//       }
+//     };
+//     fetchAllAdmin();
+//   }, []);
+
+//   // const validate = (lformData, loginData) => {
+//   //   if (!lformData.uid) {
+//   //     alert("User ID is required");
+//   //     return;
+//   //   }
+//   //   const user = loginData.find((user) => user.uid === lformData.uid);
+//   //   if (!user) {
+//   //     alert("Invalid User ID");
+//   //     return;
+//   //   }
+//   //   if (!lformData.password) {
+//   //     alert("Password is required");
+//   //     return;
+//   //   }
+//   //   if (lformData.password !== user.password) {
+//   //     alert("Wrong Password");
+//   //     return;
+//   //   }
+//   //   window.localStorage.setItem("isLoggedIn", true);
+//   //   window.localStorage.setItem("uid", lformData.uid);
+//   //   navigate("/homee");
+//   // };
+//     const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       const res = await axios.post("https://ems-imagine.onrender.com/loginforme", lformData);
+//       if (res.data.status === 'success') {
+//         window.localStorage.setItem("isLoggedIn", true);
+//         window.localStorage.setItem("uid", lformData.uid);
+//         navigate("/homee");
+//       }
+//     } catch (err) {
+//       if (err.response && err.response.data.message) {
+//         alert(err.response.data.message);
+//       } else {
+//         alert("An error occurred. Please try again.");
+//       }
+//     }
+//   };
+
+//   return (
+//     <>
+//       <div id="header">
+//         <nav>
+//           <div id="logo-img">
+//             <img src="../Images/mainlogo.png" alt="logo" />
+//           </div>
+          
+//         </nav>
+//       </div>
+//       <div className="d-flex justify-content-center align-items-center vh-100 loginPage">
+//       <div className="p-3 rounded border loginForm">
+//           <form
+//             onSubmit={(e) => {
+//               e.preventDefault();
+//               validate(lformData, loginData);
+//             }}
+//           >
+//             <h2 className="center-align text-white">EMPLOYEE</h2>
+//             <div className="mb-3">
+//               <label htmlFor="uid">
+//                 <strong>Unique Id</strong>
+//               </label>
+//               <br />
+//               <input
+//                 type="text"
+//                 autoComplete="off"
+//                 placeholder="Enter UID"
+//                 name="uid"
+//                 className="form-control"
+//                 id="uid"
+//                 value={lformData.uid}
+//                 onChange={(e) =>
+//                   setFormData({ ...lformData, uid: e.target.value })
+//                 }
+//               />
+//             </div>
+//             <div className="mb-3">
+//               <label htmlFor="password">
+//                 <strong>Password</strong>
+//               </label>
+//               <br />
+//               <input
+//                 type="password"
+//                 placeholder="Enter Password"
+//                 name="password"
+//                 className="form-control"
+//                 id="password"
+//                 value={lformData.password}
+//                 onChange={(e) =>
+//                   setFormData({ ...lformData, password: e.target.value })
+//                 }
+//               />
+//             </div>
+//             <button
+//               type="submit"
+//               className="btn btn-danger w-100 rounded-0 mb-2"
+//             >
+//               Log In
+//             </button>
+//             <button
+//             className="btn btn-secondary w-100 rounded-0 mt-2"
+//             onClick={() => navigate('/login')}
+//           >
+//             Back
+//           </button>
+//           </form>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+//export default LoginformE;
+
+
 import "../styles/Login.css";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
@@ -27,29 +171,7 @@ function LoginformE() {
     fetchAllAdmin();
   }, []);
 
-  // const validate = (lformData, loginData) => {
-  //   if (!lformData.uid) {
-  //     alert("User ID is required");
-  //     return;
-  //   }
-  //   const user = loginData.find((user) => user.uid === lformData.uid);
-  //   if (!user) {
-  //     alert("Invalid User ID");
-  //     return;
-  //   }
-  //   if (!lformData.password) {
-  //     alert("Password is required");
-  //     return;
-  //   }
-  //   if (lformData.password !== user.password) {
-  //     alert("Wrong Password");
-  //     return;
-  //   }
-  //   window.localStorage.setItem("isLoggedIn", true);
-  //   window.localStorage.setItem("uid", lformData.uid);
-  //   navigate("/homee");
-  // };
-    const handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const res = await axios.post("https://ems-imagine.onrender.com/loginforme", lformData);
@@ -57,6 +179,8 @@ function LoginformE() {
         window.localStorage.setItem("isLoggedIn", true);
         window.localStorage.setItem("uid", lformData.uid);
         navigate("/homee");
+      } else {
+        alert(res.data.message);
       }
     } catch (err) {
       if (err.response && err.response.data.message) {
@@ -74,17 +198,11 @@ function LoginformE() {
           <div id="logo-img">
             <img src="../Images/mainlogo.png" alt="logo" />
           </div>
-          
         </nav>
       </div>
       <div className="d-flex justify-content-center align-items-center vh-100 loginPage">
-      <div className="p-3 rounded border loginForm">
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              validate(lformData, loginData);
-            }}
-          >
+        <div className="p-3 rounded border loginForm">
+          <form onSubmit={handleSubmit}>
             <h2 className="center-align text-white">EMPLOYEE</h2>
             <div className="mb-3">
               <label htmlFor="uid">
@@ -128,11 +246,11 @@ function LoginformE() {
               Log In
             </button>
             <button
-            className="btn btn-secondary w-100 rounded-0 mt-2"
-            onClick={() => navigate('/login')}
-          >
-            Back
-          </button>
+              className="btn btn-secondary w-100 rounded-0 mt-2"
+              onClick={() => navigate('/login')}
+            >
+              Back
+            </button>
           </form>
         </div>
       </div>
@@ -141,3 +259,7 @@ function LoginformE() {
 }
 
 export default LoginformE;
+
+
+
+
